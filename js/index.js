d5e3cc8f1d33c7c899490f8a8e3ceb33e1f55aa0
1208470761 <1208470761@qq.com>
2020-09-21 16:34:17 +0800

    1.0

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// 图片替换
+$(window)
+    .on("resize", function () {
+        let clientWidth = $(window).width();
+        let isShowBig = clientWidth > 900;
+        let $items = $("#poster .carousel-item");
+        $items.each((index, item) => {
+            let src = isShowBig
+                ? $(item).data("lg-img")
+                : $(item).data("sm-img");
+            let imgElem = `<img src="${src}">`;
+            $(item).empty().append(imgElem);
+        });
+    })
+    .trigger("resize");
+// 主题
+$(".dark-theme").on("click", function(){
+    $("body").css({
+        backgroundColor: "black",
+        color: "white",
+    });
+    $("body a").css({
+        color: "white",
+    });
+    $(".navbar-toggler").css({
+        borderColor: "white",
+        backgroundColor: "white",
+    });
+});
+$(".light-theme").on("click", function(){
+    $("body").css({
+        backgroundColor: "white",
+        color: "black",
+    });
+    $("body a").css({
+        color: "black",
+    });
+    $(".navbar-toggler").css({
+        borderColor: "black",
+    });
+});
